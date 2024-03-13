4b31377a5ec76c54c0a0cee928c550c2ea4d5297
Sadik1099 <sadikuzzamanlalin5@gmail.com>
2024-03-13 15:57:18 +0600

    java

---
+++ math.js
@@ -0,0 +1,16 @@
+/*
+Basic math
+*/
+
+
+// variable ==> vary + able
+
+var name=5;
+var position = 13;
+var money = 435534;
+
+console.log(money);
+
+money = 3453;
+console.log(money);
+
---
+++ type.js
@@ -0,0 +1,17 @@
+// js variable type
+
+// string
+
+var name = "Md. Sadikuzzaman";
+var address = "Uttar Jhunagach Cahpanihat";
+
+console.log(name,address);
+
+// bolean 
+
+var isRich = true;
+
+console.log(isRich);
+
+// what type of variables
+console.log(typeof isRich);
