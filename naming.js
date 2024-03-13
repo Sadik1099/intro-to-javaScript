4753a53ab199f73a3e49ab7596d6c7b72c8143d2
Sadik1099 <sadikuzzamanlalin5@gmail.com>
2024-03-13 16:16:14 +0600

    naming

---
+++ naming.js
@@ -0,0 +1,27 @@
+// 1. no keyword in variable
+// no space or gap in variable
+// no quote / quotation
+//can not start with a number but number is allowed in latter
+// variable name is case sensitive
+// how to write a long variable 
+
+// number variable
+
+var orrange = 20;
+var chocolate = .5;
+var apple = '20';
+var apple2 = 20;
+
+console.log(orrange + apple);
+console.log(orrange + apple2);
+
+apple = parseInt(apple);
+console.log(apple);
+
+
+var first = .3;
+var second = .2;
+
+var total = first + second;
+console.log(total);
+console.log(total.toFixed(3));
