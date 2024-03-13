70e8645af3fddec9b9c3929374a9924d9a90b09c
Sadik1099 <sadikuzzamanlalin5@gmail.com>
2024-03-13 17:06:23 +0600

    fssd

---
+++ operation.js
@@ -0,0 +1,29 @@
+// Operation
+
+// var eggPrice = 10;
+// var onionPrice = 20;
+
+// console.log(eggPrice+onionPrice);
+// console.log(eggPrice-onionPrice);
+// console.log(eggPrice*onionPrice);
+// console.log(eggPrice/onionPrice);
+
+console.log(10/3);
+console.log(10%3);
+
+var price = 55;
+price = price+30;
+console.log(price);
+
+
+price -=50;
+price +=50;
+price *=50;
+price /=50;
+console.log(price);
+
+var first = "Sadikuzzaman";
+var second = " Lalin";
+
+var fullName = first + second;
+console.log(fullName);
